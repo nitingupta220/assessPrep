848ab5afeed11b49333930429c307c069a3b1ad8
nitingupta220 <nitin16@navgurukul.org>
2018-04-07 01:38:01 +0530

    added images

--- app.js
+++ app.js
@@ -1,20 +1,39 @@
 var app = angular.module('wordGame', []);
 app.controller('wordGameController', function ($window, $scope, $http) {
 
+    //    images to be shown
+    $scope.images = [
+        {
+            id: 1,
+            imageUrl: 'media/0.jpg'
+        }, {
+            id: 2,
+            imageUrl: 'media/1.jpg'
+        }, {
+            id: 3,
+            imageUrl: 'media/2.jpg'
+        }, {
+            id: 4,
+            imageUrl: 'media/3.jpg'
+        }
+    ];
+
+
     var appCtrl = this;
-    
-//     using localstorage to store the level of the game
+
+    //    using localStorage to store the level of the game 
     appCtrl.localStorage = $window.localStorage;
-    
     appCtrl.answer = [];
 
     var correctAnswer;
     var answerIndex = {};
 
-//    answers of different stages
-    var stagesKeywords = ['nature', 'love', 'superhero'];
 
-//    splitting the answers to compare with the predefined answers
+    //    answers of different stages of the game
+    var stagesKeywords = ['family', 'ball', 'usa', 'gift'];
+
+
+    //    function which can split all the words and return a object
     String.prototype.shuffle = function () {
         var splitString = this.split(""),
             stringLength = splitString.length,
@@ -42,29 +61,30 @@ app.controller('wordGameController', function ($window, $scope, $http) {
     var getTimes = function (num) {
         return new Array(num);
     };
-    
-    
-//    generating the hints for the question which user can select to predict the answer
+
+    //    generating hints
     var generateHints = function (searchKeyword) {
+        //        maximum words that one can enter
         var possible = "abcdefghijklmnopqrstuvwxyz";
         var remaningItems = 10 - searchKeyword.length;
 
+        //        using for loop and Math.random to randomly generating the hints and later will use this with the correctAnswer
         for (var i = 0; i < remaningItems; i++) {
             searchKeyword += possible.charAt(Math.floor(Math.random() * possible.length));
         }
 
+
         appCtrl.answerHints = searchKeyword.shuffle();
     };
 
-    
-    
-    
-
+    //    initialize the quiz
     var initQuiz = function () {
         var shuffledString;
 
+
         if ($window.localStorage.stage && $window.localStorage.stage !== "11") {
             correctAnswer = stagesKeywords[JSON.parse($window.localStorage.stage) - 1];
+            console.log(correctAnswer);
             generateHints(correctAnswer);
             initAnswerIndex(correctAnswer.length);
             appCtrl.answerLength = getTimes(correctAnswer.length);
@@ -77,10 +97,10 @@ app.controller('wordGameController', function ($window, $scope, $http) {
         }
     };
 
-//    Adding the answer to the input box
+    
+//    adding your answers in the input box
     appCtrl.addAnswers = function (key, answer) {
         var pushItemIndex = appCtrl.answer.indexOf('');
-        console.log(pushItemIndex);
         if (pushItemIndex === -1 && appCtrl.answer.length !== correctAnswer.length) {
             appCtrl.answer.push(answer);
             var answerKeyIndex = appCtrl.answer.length - 1;
@@ -94,17 +114,39 @@ app.controller('wordGameController', function ($window, $scope, $http) {
 
     };
 
-//    removing the answer from the input box
+//    removing the wrong inputs from input box
     appCtrl.removeAnswers = function (index, answer) {
         var hintsKey = answerIndex[index];
         appCtrl.answerHints[hintsKey] = answer;
         appCtrl.answer[index] = '';
     };
 
+//    submit the answer accepts if your answer is correct and reject if not
+    appCtrl.submitQuestion = function () {
+        if (appCtrl.answer.length === correctAnswer.length && appCtrl.answer.indexOf('') === -1) {
+            if (correctAnswer === appCtrl.answer.join('')) {
+                alert('Correct Answer!');
+                currentStage = JSON.parse($window.localStorage.stage);
+                if (currentStage < stagesKeywords.length) {
+                    $window.localStorage.stage = ++currentStage;
+                    appCtrl.answer = [];
+                    initQuiz();
+                } else {
+                    alert('Congrats! You finished this game. Now this game will be reset to stage 1');
+                    $window.localStorage.stage = 1;
+                    appCtrl.answer = [];
+                    initQuiz();
+                }
+            } else {
+                alert('Your answer is incorrect!');
+            }
+        } else {
+            alert('Please fill all the boxes');
+        }
+    };
 
 
 
 
     initQuiz();
-
 });
